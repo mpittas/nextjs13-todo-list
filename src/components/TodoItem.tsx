44597fc0fb7f1733ca3b3a06c22f29ea3287446c
mpittas <webpittas@gmail.com>
2023-08-02 21:51:09 +0300

    Create project and create simple todos

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,18 @@
+type TodoItemPros = {
+  id: string
+  title: string
+  complete: Boolean
+}
+export function TodoItem({ id, title, complete }: TodoItemPros) {
+  return (
+    <li className="flex gap-1 items-center">
+      <input id={id} type="checkbox" className="cursor-pointer peer" />
+      <label
+        htmlFor={id}
+        className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500"
+      >
+        {title}
+      </label>
+    </li>
+  )
+}
